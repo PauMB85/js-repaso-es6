6fca3de194a975589e7497cd895a8ecfcdce1ae2
PauMB <paumb85@gmail.com>
2019-02-16 13:11:45 +0100

    repaso a la variable var

---
+++ 1_variables_var/js/app.js
@@ -0,0 +1,18 @@
+// crear variables var
+
+var sinAsignacion;
+
+console.log('Las variables sin asignación son declaradas \'undefined\'',sinAsignacion);
+
+
+console.log('JS no hace falta declarar el tipo de dato que es \'var\'');
+var tipo = true;
+console.log('Declara la variable \'tipo\' como boolean', tipo);
+tipo = 'Hola Mundo';
+console.log('A continuación le asignamos un string', tipo);
+tipo = 20;
+console.log('Finalmente le asignamos un entero', tipo);
+
+console.log('Las variables \'var\' son las que se utilizaban anteriormente, ES5.');
+
+
