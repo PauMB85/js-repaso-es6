fbe9c11d95e069887edff731c670e7b848899a8b
PauMB <paumb85@gmail.com>
2019-02-17 21:08:39 +0100

    object destructuring

--- 6_objetos/js/app.js
+++ 6_objetos/js/app.js
@@ -28,3 +28,24 @@ console.log(persona2);
 //acceso a los atributos
 console.log(persona2.nombre);
 console.log(persona2['nombre']);
+
+
+//Prototype - asigna funciones a los objetos creados
+Persona.prototype.mostrarInformacionPersona = function() {
+    return `La Persona se llama ${this.nombre} y trabaja ${this.profesion}`;
+}
+const infoPersona = persona2.mostrarInformacionPersona();
+console.log(infoPersona);
+
+//Destructuring - forms nuevo de acceder a los atributos del objeto
+const myObjeto = {
+    version: {
+        nueva: 'ES6',
+        antigua: 'ES5'
+    }
+}
+//forma antinugua
+let versionNueva = myObjeto.version.nueva;
+console.log(versionNueva);
+let {nueva} = myObjeto.version;
+console.log(nueva);
